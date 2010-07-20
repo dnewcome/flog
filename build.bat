ef78c2fdf037d693e73ef4e8457c4af8f7e6866c
dnewcome <djn125@yahoo.com>
2010-07-20 09:47:11 -0400

    updated build script to produce debug and release versions

--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
-csc /out:flog.dll /target:library flog.cs
+mkdir debug
+csc /out:debug/Flog.dll /debug:full /target:library flog.cs
+
+mkdir release
+csc /out:release/Flog.dll /target:library flog.cs
