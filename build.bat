f69747183fae570ad9a601e2593c93c024f13fb5
dnewcome <djn125@yahoo.com>
2011-07-24 22:21:32 -0700

    added vs project file and build script

--- build.bat
+++ build.bat
@@ -1,5 +1,15 @@
-mkdir debug
-csc /out:debug/Flog.dll /debug:full /target:library flog.cs
+:: assuming 4.0 compiler is available
+set pointversion=1.0.0
 
-mkdir release
-csc /out:release/Flog.dll /target:library flog.cs
+:: set msbuild=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
+set msbuild=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe
+
+set projfile=flog.csproj
+
+:: note that we MUST specify /t:rebuild otherwise just changing the target framework isn't enough to 
+:: force uptodate logic to rebuild the project. We'll end up with 2 identical builds of whatever is first
+%msbuild% %projfile% /p:Configuration=Debug /p:TargetFrameworkVersion=v4.0 /p:PointVersion=%pointversion% /t:rebuild
+%msbuild% %projfile% /p:Configuration=Release /p:TargetFrameworkVersion=v4.0 /p:PointVersion=%pointversion% /t:rebuild
+ 
+%msbuild% %projfile% /p:Configuration=Debug /p:TargetFrameworkVersion=v2.0 /p:PointVersion=%pointversion% /t:rebuild
+%msbuild% %projfile% /p:Configuration=Release /p:TargetFrameworkVersion=v2.0 /p:PointVersion=%pointversion% /t:rebuild
