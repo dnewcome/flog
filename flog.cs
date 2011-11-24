85a630a6157bfe0404a8c1befda123d46bb3ba0c
dnewcome <djn125@yahoo.com>
2011-11-24 13:08:43 -0800

    flush handler now enables use of flush=false and still keep the last output. This is achieved by listening for process exit and flushing the log file.

--- flog.cs
+++ flog.cs
@@ -29,6 +29,14 @@ public class Flog
 {
 	private static TextWriter logger;
 	private static bool ms_initialized = false;
+	
+	/**
+	 * Flush is now configurable from code.. This was done for the test suite, not
+	 * sure yet if this violates my principles of simplicity for Flog. Rest of 
+	 * config should be set this way as well if I decide this is a good thing.
+	 */ 
+	public static bool Flush { get { return ms_flush; } set { ms_flush = value; } }
+
 	private static bool ms_flush = true;
 	private static bool ms_useConsole = false;
 	
@@ -72,21 +80,46 @@ public class Flog
 				WriteLine( "Flog flush set to " + flush.ToString() + " via app.config" );
 			}
 		} catch {}
+
+		// Flush logger when app exits
+		System.AppDomain.CurrentDomain.ProcessExit += new EventHandler( OnProcessExit );
 	}
-	
+
+	/**
+	 * Flushes the logger when the parent process exits. Disabling flush during 
+	 * execution speeds up logging ~2x. Handling process exit enables us to disable
+	 * flush after each log event and still not lose data at the end if the buffer
+	 * hasn't automatically flushed to disk.
+	 */
+	private static void OnProcessExit( object sender, EventArgs args ) {
+		logger.Flush();
+	}
+
+	/**
+	 * TODO: this assertion should be moved to separate assertion library.
+	 * It is currently here for convenience.
+	 */
 	public static void Assert( bool condition, string message ) {
 		if( condition == false ) {
 			WriteLine( message );
 		}
 	}
 		
-	// convenience to avoid uneccessary string concatenation if logging is off
+	/**
+	 * Main Log interface - provided as a convenience to avoid uneccessary 
+	 * string concatenation if logging is disabled, and to formalize via the 
+	 * method signature the various components desired in a logger message.
+	 */
 	public static void Log( string severity, string loggername, string message ) {
 		if( ms_initialized == true || ms_useConsole ) { 
 			WriteLine( severity + " " + loggername + " " + message );
 		}
 	}
 	
+	/**
+	 * WriteLine is the core output method - all other log methods are wrappers
+	 * around this one.
+	 */
 	public static void WriteLine( string message ) {
 		if( ms_initialized == true ) {
 			logger.WriteLine( DateTime.Now.ToString() + " - " + message );
@@ -94,6 +127,8 @@ public class Flog
 				logger.Flush();
 			}
 		}
+		// Warning: console is very slow, nearly 50x slower than file with 
+		// auto-flush disabled
 		if( ms_useConsole == true ) {
 				Console.WriteLine( DateTime.Now.ToString() + " - " + message );
 		}
