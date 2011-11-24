7effec5fd465a3d82dff68ef252045d7b414c80f
dnewcome <djn125@yahoo.com>
2011-11-24 13:12:57 -0800

    bumped version number up to 1.0.1

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 set msbuild=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
-set PointVersion=1.0.0
+set PointVersion=1.0.1
 
 set outpathprop=/property:OutputPath=release\%PointVersion%\2.0
 %msbuild% /property:Configuration=Debug /property:TargetFrameworkVersion=v3.5 %outpathprop%\Debug flog.csproj
